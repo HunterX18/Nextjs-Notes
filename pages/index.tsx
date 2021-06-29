c766ea90a35c870061f682bf5549d71b86475710
Smrutiranjan Sahu <srsahu13@gmail.com>
2021-06-29 20:53:52 +0530

    UI change

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,19 @@ interface Inotes {
 	_id: string;
 	title: string;
 	body: string;
+	status: string;
 	__v: Number;
 }
 
 interface IPageProps {
 	notes: Inotes[];
+	UI: Number;
+	NUI: Number;
+	UNI: Number;
+	NUNI: Number;
 }
 
-export default function Home({ notes }: IPageProps) {
+export default function Home({ notes, UI, NUI, UNI, NUNI }: IPageProps) {
 	const router = useRouter();
 	const handleDelete = (id: string) => {
 		fetch(`/api/edit/${id}`, {
@@ -26,10 +31,40 @@ export default function Home({ notes }: IPageProps) {
 			.catch((err) => console.log(err));
 	};
 	return (
-		<>
+		<div className="container-fluid min-vh-100 d-flex flex-column justify-content-center align-items-center">
 			<h1>What-To-Do</h1>
 			<Create />
-			{notes.map((note, ind) => (
+			<div className="container-xl ">
+				<div className="row">
+					<Link href="/Notes/1">
+						<div className="col m-2 border border-primary p-5 shadow-lg">
+							<h1>Urgent and Important</h1>
+							<h4>{UI} tasks pending</h4>
+						</div>
+					</Link>
+					<Link href="/Notes/2">
+						<div className="col m-2 border border-primary p-5 shadow-lg">
+							<h1>Urgent but NOT Important</h1>
+							<h4>{UNI} tasks pending</h4>
+						</div>
+					</Link>
+				</div>
+				<div className="row">
+					<Link href="/Notes/2">
+						<div className="col m-2 border border-primary p-5 shadow-lg">
+							<h1>Important but NOT Urgent</h1>
+							<h4>{NUI} tasks pending</h4>
+						</div>
+					</Link>
+					<Link href="/Notes/2">
+						<div className="col m-2 border border-primary p-5 shadow-lg">
+							<h1>Neither Important Nor Urgent</h1>
+							<h4>{NUNI} tasks pending</h4>
+						</div>
+					</Link>
+				</div>
+			</div>
+			{/* {notes.map((note, ind) => (
 				<div key={ind}>
 					<h2>
 						{ind + 1}. {note.title}
@@ -42,17 +77,33 @@ export default function Home({ notes }: IPageProps) {
 					</Link>
 					<button onClick={() => handleDelete(note._id)}>Delete</button>
 				</div>
-			))}
-		</>
+			))} */}
+		</div>
 	);
 }
 
 export const getStaticProps: GetStaticProps = async () => {
 	connect();
 	const notes = await note.find();
+	const jsonNotes = JSON.parse(JSON.stringify(notes));
+	// console.log(jsonNotes);
+	let UI = 0,
+		UNI = 0,
+		NUI = 0,
+		NUNI = 0;
+	jsonNotes.forEach((x: Inotes): void => {
+		UI += x.status == "UI" ? 1 : 0;
+		UNI += x.status == "UNI" ? 1 : 0;
+		NUI += x.status == "NUI" ? 1 : 0;
+		NUNI += x.status == "NUNI" ? 1 : 0;
+	});
 	return {
 		props: {
 			notes: JSON.parse(JSON.stringify(notes)),
+			UI,
+			UNI,
+			NUI,
+			NUNI,
 		},
 	};
 };
