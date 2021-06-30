b6f71202a33c602f59303b865356d2994a29a48d
Smrutiranjan Sahu <srsahu13@gmail.com>
2021-06-30 21:01:09 +0530

    included bootstrap icons and changed title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,10 @@ function MyApp({ Component, pageProps }: AppProps) {
 					integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
 					crossOrigin="anonymous"
 				/>
-
+				<link
+					rel="stylesheet"
+					href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
+				></link>
 				<script
 					src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
 					integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
@@ -20,6 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 					async
 					defer
 				></script>
+				<title>What-To-Do</title>
 			</Head>
 			<Component {...pageProps} />
 		</>
