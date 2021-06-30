33bf60efbd435d91c9da28a644dfe92c74aa72ff
Smrutiranjan Sahu <srsahu13@gmail.com>
2021-06-30 21:00:26 +0530

    minor change

--- components/db.ts
+++ components/db.ts
@@ -4,10 +4,11 @@ const { MONGO_URI } = require("../keys");
 const connect = () => {
 	if (mongoose.connections[0].readyState) return;
 	mongoose
-		.connect(
-			MONGO_URI,
-			{ useNewUrlParser: true, useUnifiedTopology: true }
-		)
+		.connect(MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: true,
+		})
 		.then(() => console.log("connected"))
 		.catch((err: any) => console.log(err));
 };
