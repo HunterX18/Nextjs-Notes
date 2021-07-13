81806e867ba4038ab1b60919e877d79969c7fb72
Smrutiranjan Sahu <srsahu13@gmail.com>
2021-07-13 15:09:30 +0530

    added env variable

--- components/db.ts
+++ components/db.ts
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-const { MONGO_URI } = require("../keys");
 
 const connect = () => {
 	if (mongoose.connections[0].readyState) return;
 	mongoose
-		.connect(MONGO_URI, {
+		.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useFindAndModify: true,
